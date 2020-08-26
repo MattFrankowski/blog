dc21926bb54786bda4390e43c53e6f450a1bd95a
MattFrankowski <mateofrankowski@gmail.com>
2020-08-26 21:57:41 +0200

    Navbar update

--- website/blog/forms.py
+++ website/blog/forms.py
@@ -1,11 +1,11 @@
 from django.forms import ModelForm
-
-
+from django import forms
+from django.forms.widgets import PasswordInput, TextInput
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 
 from .models import Post, Blogger
-
 
 
 class PostForm(ModelForm):
@@ -14,9 +14,15 @@ class PostForm(ModelForm):
         fields = ['title', 'content', 'author']
 
 
-
 class UserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+''' Override built-in Login Form '''
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=TextInput(attrs={'class': 'validate', 'placeholder': 'Email'}))
+    password = forms.CharField(widget=PasswordInput(attrs={'placeholder': 'Password'}))
+
 
