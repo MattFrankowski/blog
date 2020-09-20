a7cf8e455118691c55fba4b500a42f17acd06277
MattFrankowski <mateofrankowski@gmail.com>
2020-09-20 18:30:48 +0200

    Flush database

--- website/blog/models.py
+++ website/blog/models.py
@@ -28,7 +28,7 @@ class Post(models.Model):
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    content = models.CharField(max_length=100, null=True)
+    content = models.CharField(max_length=350, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
